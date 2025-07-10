02bd8f8b0208198f836141c8cd0caee44c7dabed
klu2200032929 <2200032929@kluniversity.in>
2025-07-10 21:39:01 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import BankDetails from "./pages/BankDetails";
+import ContactUs from "./pages/ContactUs";
+import Navbar from "./components/Navbar";
+import "./App.css";
 
 function App() {
+  const [goldPrice, setGoldPrice] = useState(null);
+
+ useEffect(() => {
+  const fetchGoldPrice = async () => {
+    try {
+      const res = await fetch("http://localhost:5000/api/live-gold");
+      const data = await res.json();
+      setGoldPrice(data.price);
+    } catch (err) {
+      console.error("Failed to fetch live gold rate:", err);
+    }
+  };
+
+  fetchGoldPrice();
+}, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+     {goldPrice && (
+  <div className="live-price-bar">
+    Live Gold Price (MCX): {goldPrice}
+  </div>
+)}
+
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/bank-details" element={<BankDetails />} />
+        <Route path="/contact-us" element={<ContactUs />} />
+      </Routes>
+    </Router>
   );
 }
 
